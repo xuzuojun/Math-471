75aad9f64e667427fb7d4373492fa652af4119fa
Divakar Viswanath <divakar@QX411>
2014-01-22 07:12:01 -0500

    matlab/lgrng.m and matlab/rungel.m

--- python/interp/run_demo.py
+++ python/interp/run_demo.py
@@ -5,5 +5,5 @@ def runge(x):
 	return 1.0/(1+x*x)
 
 x = gridpts.cheb(100)
-xx = gridpts.unif(200)*0.99
+xx = gridpts.unif(200)*0.99+1e-4
 demo.plot(runge, x, xx)
--- python/interp/tests/test_lgrng.py
+++ python/interp/tests/test_lgrng.py
@@ -12,7 +12,7 @@ y = np.sin(np.sin(x))
 plt.plot(x, y, 'ro')
 
 xx = gridpts.unif(200)
-xx = xx + 1e-6
+xx = xx*0.999 + 1e-6
 yy = lgrng.interp(x, y, xx)
 plt.plot(xx, yy, 'k')
 plt.axis('tight')
